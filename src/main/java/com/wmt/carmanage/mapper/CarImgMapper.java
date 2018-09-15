692131d0aa5f25229c07adf30bcd9820d0061ce3
wumt <827319577@qq.com>
2018-09-15 09:45:00 +0800

    第9次

---
+++ src/main/java/com/wmt/carmanage/mapper/CarImgMapper.java
@@ -0,0 +1,16 @@
+package com.wmt.carmanage.mapper;
+
+import com.wmt.carmanage.entity.CarImg;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author wumt
+ * @since 2018-09-14
+ */
+public interface CarImgMapper extends BaseMapper<CarImg> {
+
+}
